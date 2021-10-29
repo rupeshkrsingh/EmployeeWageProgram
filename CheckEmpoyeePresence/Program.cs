d5374b523c594fa2d9ba066c4f57be3bbb358302
rupeshsingh.ags@gmail.com <rupeshsingh.ags@gmail.com>
2021-10-29 18:37:21 +0530

    commite

--- CheckEmpoyeePresence/Program.cs
+++ CheckEmpoyeePresence/Program.cs
@@ -2,11 +2,21 @@
 
 namespace CheckEmpoyeePresence
 {
-    class Program
+   public class Program
     {
-        static void Main(string[] args)
+       public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int isFullTime = 1;
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == isFullTime)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
