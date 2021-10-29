28b5b490ba3e57c8f351dbe85e95055e70091f2a
rupeshsingh.ags@gmail.com <rupeshsingh.ags@gmail.com>
2021-10-29 18:52:39 +0530

    Calculate Emplyee Wage

--- EmployeeWageProgram/Program.cs
+++ EmployeeWageProgram/Program.cs
@@ -1,12 +1,30 @@
 ﻿using System;
-
 namespace EmployeeWageProgram
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //constant 
+            int isFullTime = 1;
+            int empRatePerHour = 20;
+
+            //variable
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 2);
+            if (empCheck == isFullTime)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * empRatePerHour;
+            Console.WriteLine("Emp Wage:" + empWage);
         }
     }
 }
