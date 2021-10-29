28fb1b149c117e772c4e00de413a82f9a6dcad89
rupeshsingh.ags@gmail.com <rupeshsingh.ags@gmail.com>
2021-10-29 19:55:03 +0530

    demo code commited

--- EmployeeWageProgram/Program.cs
+++ EmployeeWageProgram/Program.cs
@@ -7,6 +7,7 @@ namespace EmployeeWageProgram
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Demo Branch!");
         }
     }
 }
