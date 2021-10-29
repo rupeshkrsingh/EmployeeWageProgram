9ce9fb2b252e898b0e53a9c34780988f99aac09a
rupeshsingh.ags@gmail.com <rupeshsingh.ags@gmail.com>
2021-10-29 19:02:20 +0530

    Part Time Emplyee commet

--- EmployeeWageProgram/Program.cs
+++ EmployeeWageProgram/Program.cs
@@ -2,11 +2,35 @@
 
 namespace EmployeeWageProgram
 {
-    class Program
+   public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //constant 
+            int isFullTime = 2;
+            int isPartTime = 1;
+            int empRatePerHour = 20;
+
+            //variable
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(0, 3);
+            if(empCheck==isPartTime)
+            {
+                empHrs = 4;
+            }
+            else if (empCheck == isFullTime)
+            {
+                empHrs = 8;
+            }
+            else
+            {
+                empHrs = 0;
+            }
+            empWage = empHrs * empRatePerHour;
+            Console.WriteLine("Emp Wage:" + empWage);
         }
     }
 }
